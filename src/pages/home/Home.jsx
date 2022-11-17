52a3f34a79525930606f71b1f38d2ba2dbf061c6
locbh3010 <locbh3010@gmail.com>
2022-11-17 23:55:08 +0700

    them slide cho trang chu

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,6 +4,11 @@ import { db } from "../../configs/firebase-configs";
 import { collection } from "firebase/firestore";
 import { Link } from "react-router-dom";
 import { ProductItem, ProductList } from "../../components/ui/product/Product";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Navigation, Autoplay } from "swiper";
 
 const Home = () => {
   const colRef = query(collection(db, "products"), limit(8));
@@ -25,14 +30,29 @@ const Home = () => {
     <div className="w-full">
       <div className="container">
         <div className="rounded-2xl overflow-hidden relative">
-          {products?.length > 0 && (
-            <img
-              src={products[0].images[0]}
-              alt=""
-              className="w-full h-[70vh] object-cover rounded-2xl"
-            />
-          )}
-
+          <Swiper
+            pagination={{
+              type: "progressbar",
+            }}
+            navigation={true}
+            modules={[Pagination, Navigation, Autoplay]}
+            className="w-full flex h-[60vh]"
+            autoplay={{
+              delay: 1500,
+              disableOnInteraction: false,
+            }}
+          >
+            {products?.length > 0 &&
+              products.map((product) => (
+                <SwiperSlide key={product.id}>
+                  <img
+                    src={product.images[0]}
+                    alt=""
+                    className="w-full h-full object-cover"
+                  />
+                </SwiperSlide>
+              ))}
+          </Swiper>
           <div className="absolute left-10 top-1/2 -translate-y-1/2 text-white">
             <h1 className="text-4xl font-medium mb-4">{products[0]?.name}</h1>
             <span className="text-[26px]">${products[0]?.price}</span>
