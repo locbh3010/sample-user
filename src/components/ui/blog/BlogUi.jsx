58009618656b91f8f8e450a1f2346fd0dee85794
locbh3010 <locbh3010@gmail.com>
2022-11-18 00:12:40 +0700

    hoan thanh giao dien blog item, blog page

--- src/components/ui/blog/BlogUi.jsx
+++ src/components/ui/blog/BlogUi.jsx
@@ -1,5 +1,33 @@
 import React from "react";
 
 export const BlogItem = () => {
-  return <div></div>;
+  return (
+    <div className="flex flex-col group gap-5.5">
+      <div className="aspect-video overflow-hidden rounded-lg flex-shrink-0">
+        <img
+          src="https://i.pinimg.com/564x/ab/50/40/ab5040a1dd86289dd175ab4dce78d598.jpg"
+          alt=""
+          className="w-full h-full object-cover duration-300 group-hover:scale-125"
+        />
+      </div>
+      <div className="flex flex-col flex-1">
+        <span className="block text-gray-dark text-sm capitalize">
+          Fashion - October 8, 2020
+        </span>
+        <p className="mt-1 mb-3.5 capitalize text-xl line-clamp-2 text-black">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, ab.
+        </p>
+        <span className="text-gray-dark mb-6 line-clamp-2 mt-auto">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Praesentium
+          ratione maxime fugiat tempore atque at. Lorem, ipsum dolor sit amet
+          consectetur adipisicing elit. Ullam iste cum ipsum! Incidunt voluptate
+          id dicta cum iusto provident alias?
+        </span>
+
+        <span className="mt-auto text-accent capitalize font-bold">
+          Read More
+        </span>
+      </div>
+    </div>
+  );
 };
--- src/pages/blog/Blogs.jsx
+++ src/pages/blog/Blogs.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { BlogItem } from "../../components/ui/blog/BlogUi";
 
 const Blogs = () => {
-  return <div></div>;
+  return (
+    <div className="container pt-24 pb-[250px]">
+      <div className="grid grid-cols-3 gap-12">
+        <BlogItem />
+        <BlogItem />
+        <BlogItem />
+      </div>
+    </div>
+  );
 };
 
 export default Blogs;
