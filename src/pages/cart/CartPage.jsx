daa8c64b64036462fc933f4da1b1d068e5b8fdd3
locbh3010 <locbh3010@gmail.com>
2022-11-20 22:47:33 +0700

    upload order sumary

--- src/pages/cart/CartPage.jsx
+++ src/pages/cart/CartPage.jsx
@@ -1,33 +1,33 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import CloseIcon from "../../components/icon/CloseIcon";
 import Button from "../../components/ui/button/Button";
 
-const CartItem = () => {
-  return (
-    <div className="flex items-start gap-10 w-full relative pb-10 border-b border-gray-light">
-      <div className="overflow-hidden rounded aspect-square w-[15%]">
-        <img
-          src="https://i.pinimg.com/236x/d0/f8/b8/d0f8b804a908ce4aaee63d54035d2192.jpg"
-          alt=""
-          className="w-full h-full object-cover"
-        />
-      </div>
-      <div>
-        <p className="text-xl text-black font-medium mb-3.5 capitalize">
-          Lira Earrings
-        </p>
-        <p className="text-gray-dark">Black / Medium</p>
-        <span className="text-accent">$20</span>
-      </div>
-
-      <button className="absolute top-0 right-0">
-        <CloseIcon width="24px" />
-      </button>
-    </div>
-  );
-};
-
 const CartPage = () => {
+  const CartItem = () => {
+    return (
+      <div className="flex items-start gap-10 w-full relative pb-10 border-b border-gray-light">
+        <div className="overflow-hidden rounded aspect-square w-[15%]">
+          <img
+            src="https://i.pinimg.com/236x/d0/f8/b8/d0f8b804a908ce4aaee63d54035d2192.jpg"
+            alt=""
+            className="w-full h-full object-cover"
+          />
+        </div>
+        <div>
+          <p className="text-xl text-black font-medium mb-3.5 capitalize">
+            Lira Earrings
+          </p>
+          <p className="text-gray-dark">Black / Medium</p>
+          <span className="text-accent">$20</span>
+        </div>
+
+        <button className="absolute top-0 right-0">
+          <CloseIcon width="24px" />
+        </button>
+      </div>
+    );
+  };
   return (
     <div className="py-24 pb-[250px]">
       <div className="container">
@@ -38,7 +38,9 @@ const CartPage = () => {
           <div className="font-bold uppercase flex-1 flex-shrink-0 basis-[70%]">
             total: $10
           </div>
-          <Button type="secondary">PROCEED TO CHECKOUT</Button>
+          <Button type="secondary">
+            <Link to="/sumary">PROCEED TO CHECKOUT</Link>
+          </Button>
         </div>
         <div className="max-w-4xl mx-auto grid grid-cols-1 grid-flow-row auto-rows-fr gap-10">
           <CartItem />
